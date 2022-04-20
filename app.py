bf34a0340ebe1a14d333efcbea3e1bb54e3e3d03
Vaibhav Prakash <58112154+vbhvprksh@users.noreply.github.com>
2022-04-20 22:24:54 +0530

    Till Synopsis
    Project is done till synopsis now Only major changes will be taken place.

--- app.py
+++ app.py
@@ -1,4 +1,148 @@
+from django.shortcuts import render
 from app import app
+from flask import render_template,request,redirect
+import sqlite3
+import os
+
+
+#Routes
+
+#Home
+
+# Route to home page
+@app.route("/")
+def index():
+    return render_template('index.html')
+
+
+
+
+
+#Login Routes BEGIN
+
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
+
+@app.route("/login",methods=["POST"])
+def checklogin():
+    UN=request.form['username']
+    PD=request.form['password']
+    currentlocation= os.path.dirname(os.path.abspath(__file__))
+    sqlconnection=sqlite3.connect(currentlocation + "\Login.db")
+    cursor=sqlconnection.cursor()
+    query1="SELECT Username,Password from Users WHERE Username= '{un}' AND Password= '{pw}' ".format(un=UN,pw=PD)
+    rows = cursor.execute(query1)
+    rows=rows.fetchall()
+    if len(rows) ==1:
+        return render_template("loggedin.html")
+    else:
+        return redirect("/register")
+
+
+#Login Routes End
+
+#Register Routes Begin
+
+
+@app.route("/register",methods=["GET","POST"])
+
+def registerpage():
+    if request.method == "POST":
+        print("In the post method")
+        DUN=request.form['DUsername']
+        DPD=request.form['pwd']
+        Uemail=request.form['EmailUser']
+        print(DUN)
+        print(DPD)
+        print(Uemail)
+        currentlocation= os.path.dirname(os.path.abspath(__file__))
+        sqlconnection=sqlite3.connect(currentlocation + "\Login.db")
+        cursor=sqlconnection.cursor()
+        query1="INSERT INTO Users VALUES('{u}','{p}','{e}')".format(u=DUN,p=DPD,e=Uemail)
+        cursor.execute(query1)
+        print("Data is inserted Successfully")
+        sqlconnection.commit()
+        return redirect("/")
+    else:
+        print("In the render Page")
+        return render_template("Register.html")
+
+#Register Routes End here
+
+
+
+
+#Loggedin Routes Starts here
+@app.route("/loggedin",methods=["GET","POST"])
+
+def logged():
+    if request.method == "POST":
+        print("In the post method")
+        Fname=request.form['fname']
+        Lname=request.form['lname']
+        Lemail=request.form['lemail']
+        mobile=request.form['mobile']
+        date=request.form['date']
+        
+        print(Fname)
+        print(Lname)
+        print(Lemail)
+        print(mobile)
+        print(date)
+        currentlocation= os.path.dirname(os.path.abspath(__file__))
+        sqlconnection=sqlite3.connect(currentlocation + "\Login.db")
+        cursor=sqlconnection.cursor()
+        query1="INSERT INTO Loggedin VALUES('{fn}','{ln}','{le}','{mob}','{dt}')".format(fn=Fname,ln=Lname,le=Lemail,mob=mobile,dt=date)
+        cursor.execute(query1)
+        print("Data is inserted Successfully")
+        sqlconnection.commit()
+        return redirect("/doc")
+    else:
+        print("In the render Page")
+        return render_template("loggedin.html")
+
+
+
+
+#Loggedin Route Ends here
+
+
+#Doc Uploading
+
+
+@app.route("/doc")
+def doc():
+    return render_template("doc.html")
+
+
+
+
+#Thank you Route Starts here
+
+@app.route("/thank")
+def thankyou():
+    return render_template("thankyou.html")
+
+@app.route("/blogs")
+def blogs():
+    return render_template("blogs.html")
+
+
+
+
+
+#EmailOtp Validation Starts here
+
+
+#EmailOtp Validation Ends here
+
+
+#Pan Verification
+
+
+
 
 if __name__ == '__main__':
-   app.run()
+    app.run()
--- app/views.py
+++ app/views.py
@@ -1,5 +1,4 @@
 # Important imports
-from django.shortcuts import render
 from app import app
 from flask import request, render_template
 import os
@@ -13,14 +12,10 @@ app.config['INITIAL_FILE_UPLOADS'] = 'app/static/uploads'
 app.config['EXISTNG_FILE'] = 'app/static/original'
 app.config['GENERATED_FILE'] = 'app/static/generated'
 
-@app.route('/login')
-def login():
-    return render_template("login.html")
+#PAN card
 
-
-# Route to home page
-@app.route("/", methods=["GET", "POST"])
-def index():
+@app.route("/pan", methods=["GET", "POST"])
+def pan():
 
 	# Execute if request is get
 	if request.method == "GET":
@@ -29,7 +24,7 @@ def index():
 	# Execute if reuqest is post
 	if request.method == "POST":
                 # Get uploaded image
-                file_upload = request.files['file_upload']
+                file_upload = request.files['file_uploading']
                 filename = file_upload.filename
                 
                 # Resize and save the uploaded image
@@ -69,10 +64,58 @@ def index():
                 cv2.imwrite(os.path.join(app.config['GENERATED_FILE'], 'image_diff.jpg'), diff)
                 cv2.imwrite(os.path.join(app.config['GENERATED_FILE'], 'image_thresh.jpg'), thresh)
                 return render_template('index.html',pred=str(round(score*100,2)) + '%' + ' correct')
+
+
+@app.route("/aadhar", methods=["GET", "POST"])
+def aadhar():
+
+	# Execute if request is get
+	if request.method == "GET":
+	    return render_template("doc.html")
+
+	# Execute if reuqest is post
+	if request.method == "POST":
+                # Get uploaded image
+                file_upload = request.files['file_uploadd']
+                filename = file_upload.filename
                 
+                # Resize and save the uploaded image
+                uploaded_image = Image.open(file_upload).resize((250,160))
+                uploaded_image.save(os.path.join(app.config['INITIAL_FILE_UPLOADS'], 'aadhar.jpg'))
 
+                # Resize and save the original image to ensure both uploaded and original matches in size
+                original_image = Image.open(os.path.join(app.config['EXISTNG_FILE'], 'aadhar.jpg')).resize((250,160))
+                original_image.save(os.path.join(app.config['EXISTNG_FILE'], 'aadhar.jpg'))
 
+                # Read uploaded and original image as array
+                original_image = cv2.imread(os.path.join(app.config['EXISTNG_FILE'], 'aadhar.jpg'))
+                uploaded_image = cv2.imread(os.path.join(app.config['INITIAL_FILE_UPLOADS'], 'aadhar.jpg'))
 
+                # Convert image into grayscale
+                original_gray = cv2.cvtColor(original_image, cv2.COLOR_BGR2GRAY)
+                uploaded_gray = cv2.cvtColor(uploaded_image, cv2.COLOR_BGR2GRAY)
+
+                # Calculate structural similarity
+                (score, diff) = structural_similarity(original_gray, uploaded_gray, full=True)
+                diff = (diff * 255).astype("uint8")
+
+                # Calculate threshold and contours
+                thresh = cv2.threshold(diff, 0, 255, cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)[1]
+                cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+                cnts = imutils.grab_contours(cnts)
+                
+                # Draw contours on image
+                for c in cnts:
+                    (x, y, w, h) = cv2.boundingRect(c)
+                    cv2.rectangle(original_image, (x, y), (x + w, y + h), (0, 0, 255), 2)
+                    cv2.rectangle(uploaded_image, (x, y), (x + w, y + h), (0, 0, 255), 2)
+
+                # Save all output images (if required)
+                cv2.imwrite(os.path.join(app.config['GENERATED_FILE'], 'image_original.jpg'), original_image)
+                cv2.imwrite(os.path.join(app.config['GENERATED_FILE'], 'image_uploaded.jpg'), uploaded_image)
+                cv2.imwrite(os.path.join(app.config['GENERATED_FILE'], 'image_diff.jpg'), diff)
+                cv2.imwrite(os.path.join(app.config['GENERATED_FILE'], 'image_thresh.jpg'), thresh)
+                return render_template('doc.html',predd=str(round(score*100,2)) + '%' + ' correct')
 
 
 # Main function
